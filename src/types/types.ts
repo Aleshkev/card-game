c6e70b8992a9da5001c69fdbbb0f1665e2219860
Jonasz Aleszkiewicz <43296251+Aleshkev@users.noreply.github.com>
2025-01-01 19:06:15 +0100

    Remove jokers

--- src/types/types.ts
+++ src/types/types.ts
@@ -1,46 +1,32 @@
 import { count, enumFromTo } from "../utilities/functional"
 
+let nextId = 1
+const newId = () => nextId++
 
-export type CardColor = number & { _: "CardColor" }
-export const minCardColor = 1
-export const maxCardColor = 4
-export function toCardColor(x: number): CardColor {
-  console.assert(1 <= x && x <= 4)
-  return x as CardColor
-}
-
-export type CardValue = number & { _: "CardValue" }
-export const minCardValue = 1
-export const maxCardValue = 13
-export function toCardValue(x: number): CardValue {
-  console.assert(minCardValue <= x && x <= maxCardValue)
-  return x as CardValue
-}
 
 export type CardId = number & { _: "CardId" }
-let nextCardId = 0
-export function newCardId(): CardId {
-  return (nextCardId++) as CardId
-}
+export const newCardId = () => newId() as CardId
 
-export type JokerCard = {
-  kind: "Joker"
+export type Card = {
   id: CardId
-}
-export type RegularCard = {
-  kind: "Regular"
-  id: CardId
-  color: CardColor
-  value: CardValue
-}
-export type Card = JokerCard | RegularCard
+  value: number
+  color: number
+} & { _: "Card" }
+export const minCardValue = 1
+export const maxCardValue = 13
+export const minCardColor = 1
+export const maxCardColor = 4
+export const newCard: ((x: { value: number, color: number }) => Card) =
+  ({ value, color }) => {
+    console.assert(minCardValue <= value && value <= maxCardValue)
+    console.assert(minCardColor <= color && color <= maxCardColor)
+    return { id: newCardId(), value, color } as Card
+  }
 
 export function sortCards(cards: Card[]): Card[] {
   const r = cards.slice()
   r.sort((a, b) => {
-    let [aValue, aColor] = a.kind === "Joker" ? [-1, -1] : [a.value, a.color]
-    let [bValue, bColor] = b.kind === "Joker" ? [-1, -1] : [b.value, b.color]
-    return aColor - bColor || aValue - bValue
+    return a.value - b.value || a.color - b.color
   })
   return r
 }
@@ -84,12 +70,9 @@ export type RoundState = {
 
 export function standardDeck(): Card[] {
   let cards: Card[] = []
-  for (let i = 0; i < 4; ++i) {
-    cards.push({ kind: "Joker", id: newCardId() })
-  }
   for (let color = 1; color <= 4; ++color) {
     for (let value = 1; value <= 13; ++value) {
-      cards.push({ kind: "Regular", id: newCardId(), color: toCardColor(color), value: toCardValue(value) })
+      cards.push(newCard({ color, value }))
     }
   }
   return cards
@@ -116,20 +99,14 @@ export function randomItems(n: number): Item[] {
 // }
 
 
-export function getScore(hand: Card[]): number {
+export function getScore(cards: Card[]): number {
 
   const recognised: string[] = []
   const scores = []
 
-  const nJokers = hand.filter((x) => x.kind === "Joker").length
-  const cards = hand.filter((x) => x.kind === "Regular").map(x => ({ value: x.value, color: x.color }))
-
   for (let start = minCardValue; start <= maxCardValue; ++start) {
-    let misses = 0
     for (let stop = start; stop <= maxCardValue; ++stop) {
-      if (!cards.some(({ value }) => value === stop)) ++misses;
-      if (misses > nJokers) break;
-      if (stop < 1) continue;
+      if (!cards.some(({ value }) => value === stop)) break;
       const length = stop - start + 1
       recognised.push(length > 1 ? `${length}-flush (${stop} high card)` : `${stop} high card`)
       scores.push((stop - start + 1) * 1000 + 100 + stop)
@@ -143,13 +120,12 @@ export function getScore(hand: Card[]): number {
         ++nColors;
       }
     }
-    nColors = Math.min(maxCardColor - minCardColor + 1, nColors + nJokers)
     if (nColors < 2) continue;
     recognised.push(`${nColors} of a kind (${val} high card)`)
     scores.push(nColors * 1000 + 200 + val)
   }
 
-  console.log(hand, recognised)
+  console.log(cards, recognised)
   return Math.max(...scores)
 }
 
