dbf9375db607c1e7ba06e6664ca1d2e6afe377c8
Jonasz Aleszkiewicz <43296251+Aleshkev@users.noreply.github.com>
2025-01-02 23:11:41 +0100

    Show a card placeholder if a card grid has no cards to display

--- src/components/CardGridView.tsx
+++ src/components/CardGridView.tsx
@@ -1,4 +1,5 @@
 import { Card, sortCards } from "../types/types";
+import { CardPlaceholder } from "./CardPlaceholder";
 import { CardView } from "./CardView";
 
 export type Props = {
@@ -8,13 +9,12 @@ export type Props = {
 export function CardGridView({ cards }: Props) {
   const cardsSorted = sortCards(cards);
   return (
-    <div
-      className=" mx-auto flex flex-col gap-2"
-    >
+    <div className=" mx-auto flex flex-col gap-2">
       <div className="flex flex-row flex-wrap gap-2 ">
-        {cardsSorted.map((card) => (
-          <CardView key={card.id} card={card} />
-        ))}
+        {(cardsSorted.length &&
+          cardsSorted.map((card) => (
+            <CardView key={card.id} card={card} />
+          ))) || <CardPlaceholder />}
       </div>
     </div>
   );
